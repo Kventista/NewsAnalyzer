d27e514ec3ece626e4ce3efe750c21de6e5d51c7
Xenia Makarova <r-168@ya.ru>
2019-11-22 16:47:38 +0300

    cross убрала из вебпак
    зафиксировала версии плагинов
    добавила описание в README.md
    слайдер без лишних классов и header в темной теме

--- src/about-project.js
+++ src/about-project.js
@@ -5,7 +5,7 @@ import "./vendor/flickity.css"
 const serverUrl = NODE_ENV === 'development' ? 'http://praktikum.tk/cohort3' : 'https://praktikum.tk/cohort3';
 
 let Flickity = require('flickity');
-let flkty = new Flickity('.flickity_scroll', {
+let flkty = new Flickity('.flickity', {
     // options
     cellAlign: 'left',
     contain: true,
